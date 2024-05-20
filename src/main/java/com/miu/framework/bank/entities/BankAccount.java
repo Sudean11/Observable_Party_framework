fef2b5efa1fe5b5b246e47ea96fd5646dfcc17d3
kal <kalkidan.alemu@miu.edu>
2024-05-20 17:08:17 -0500

    more properties for bank account

--- src/main/java/com/miu/framework/bank/entities/BankAccount.java
+++ src/main/java/com/miu/framework/bank/entities/BankAccount.java
@@ -1,17 +1,50 @@
 package com.miu.framework.bank.entities;
+
+import java.util.Date;
+
 public abstract class BankAccount {
     private String accountNumber;
     private double amount;
+    private String street;
+    private String city;
+    private String state;
+    private String zip;
+    private Date birthdate;
 
-    public BankAccount(String accountNumber) {
+    public BankAccount(String accountNumber, double amount, String street, String city, String state, String zip, Date birthdate) {
         this.accountNumber = accountNumber;
-        this.amount = 0.0;
+        this.amount = amount;
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.birthdate = birthdate;
     }
+
     public abstract void createAccount(String accountNumber);
     public void withdraw(double amount) {
 
     }
 
+    public String getStreet() {
+        return street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
 
     public void deposit(double amount) {
 
