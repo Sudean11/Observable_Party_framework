91af93ebaceb659d56cd180728af5d8df20049ef
Sudin <sudean.mdrr@gmail.com>
2024-05-20 15:58:33 -0500

    setup done

---
+++ src/main/java/com/miu/framework/main/ClassInterface.java
@@ -0,0 +1,2 @@
+package edu.mum.cs.cs525.labs.exercises.project.src.main.java.com.miu.framework.main;public class ClassInterface {
+}
--- ui/framework/Main.java
+++
@@ -1,2 +0,0 @@
-package edu.mum.cs.cs525.labs.exercises.project.ui.framework;public class Main {
-}
