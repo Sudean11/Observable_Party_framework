b0b342b8628babf6486833e052c9cd8e370d3641
Sudin <sudean.mdrr@gmail.com>
2024-05-21 13:46:30 -0500

    project setup

--- src/main/java/com/miu/framework/creditCard/Repositories/AccountDAO.java
+++ src/main/java/com/miu/framework/creditCard/Repositories/AccountDAO.java
@@ -12,18 +12,18 @@ public  abstract  class AccountDAO {
 		saveAccount(account);
 	};
 	public void updateAccount(Account account){
-		Account accountexist = loadAccount(account.getAccountNumber());
-		if (accountexist != null) {
-			accounts.remove(accountexist); // remove the old
-			accounts.add(account); // add the new
-		}
+//		Account accountexist = loadAccount(account.getAccountNumber());
+//		if (accountexist != null) {
+//			accounts.remove(accountexist); // remove the old
+//			accounts.add(account); // add the new
+//		}
 	};
 	public Account loadAccount(String accountnumber){
-		for (Account account : accounts) {
-			if (account.getAccountNumber() == accountnumber) {
-				return account;
-			}
-		}
+//		for (Account account : accounts) {
+//			if (account.getAccountNumber() == accountnumber) {
+//				return account;
+//			}
+//		}
 		return null;
 	};
 	public Collection<Account> getAccounts(){
