219f1a469b547e1cf6808179321cb590108f0859
prayash27 <prayash.shrestha@wiseyak.com>
2024-05-21 21:26:57 -0500

    removed unwanted

--- src/main/java/com/miu/framework/common/command/WithdrawCommand.java
+++ src/main/java/com/miu/framework/common/command/WithdrawCommand.java
@@ -1,11 +1,6 @@
 package com.miu.framework.common.command;
 
 public class WithdrawCommand implements Command {
-    private AccountService accountService;
-
-    public WithdrawCommand(AccountService accountService) {
-        this.accountService = accountService;
-    }
 
     @Override
     public void execute() {
