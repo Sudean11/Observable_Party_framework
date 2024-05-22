2d107520921ca495826fbdbcaf26641ff27b9fa0
ayoubkoreteszx <65472358+ayoubkoreteszx@users.noreply.github.com>
2024-05-21 23:13:26 -0500

    fix merge for generate report

--- src/main/java/com/miu/framework/creditCard/entities/CreditCardStrategy.java
+++ src/main/java/com/miu/framework/creditCard/entities/CreditCardStrategy.java
@@ -1,12 +1,11 @@
 package com.miu.framework.creditCard.entities;
 
-import com.miu.framework.bank.entities.AccountTypeStrategy;
 import com.miu.framework.bank.entities.Transaction;
 import com.miu.framework.bank.entities.TransactionType;
 import com.miu.framework.common.entity.Account;
 import com.miu.framework.common.entity.Party;
 
-public interface CreditCardStrategy extends AccountTypeStrategy {
+public interface CreditCardStrategy  {
     double getMonthlyInterest();
 
     double getMinimumPayment();
